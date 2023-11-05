0e7e51a2b75bfc9ea846033df3dd6780737b0f80
martinmonzo <martinmgancia1@gmail.com>
2023-11-05 02:01:27 -0300

    Save params in localStorage

--- js/formulario.js
+++ js/formulario.js
@@ -1,6 +1,8 @@
 function validarInputs() {
     const jugador1 = document.getElementById("jugador1");
     const jugador2 = document.getElementById("jugador2");
+    const segundos = document.getElementById("segundos");
+    const comodines = document.getElementById("comodines");
 
     if (!jugador1.value) {
         jugador1.setCustomValidity('Por favor, complete este campo.');
@@ -17,5 +19,9 @@ function validarInputs() {
         return false;
     }
 
+    localStorage.setItem('jugador1', jugador1.value);
+    localStorage.setItem('jugador2', jugador2.value);
+    localStorage.setItem('segundos', segundos.value);
+    localStorage.setItem('comodines', comodines.value);
     return true;
 }
--- js/main.js
+++ js/main.js
@@ -2,17 +2,17 @@ let dom, reloj, roscoActivo, partida;
 
 
 function iniciarJuego() {
-    const url = new URL(window.location.href);
-    const parametros = url.searchParams;
+    var urlParams = new URLSearchParams(window.location.search);
+    var esFormulario = urlParams.has('form');
 
-    const jugador1 = parametros.get('jugador1');
-    const jugador2 = parametros.get('jugador2');
-    const segundos = parametros.get('segundos');
-    const comodines = parametros.get('comodines');
-
+    const jugador1 = localStorage.getItem('jugador1');
+    const jugador2 = localStorage.getItem('jugador2');
+    const segundos = localStorage.getItem('segundos');
+    const comodines = localStorage.getItem('comodines');
     
-    if (!jugador1 || !jugador2 || !segundos || comodines == null) {
-        window.location.href = window.location.href.replace('rosco.html', 'index.html').replace(window.location.search, '');
+    if (!esFormulario) {
+        const redirect = window.location.href.replace('rosco.html', 'index.html').replace(window.location.search, '')
+        window.open(redirect, '_self');
     }
 
     dom = new DOM();
