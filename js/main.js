8ad7810c5a116fbc761b0eb9fce68167d2922d97
martinmonzo <martinmgancia1@gmail.com>
2023-11-05 02:36:47 -0300

    Detect if is called from form

--- js/main.js
+++ js/main.js
@@ -4,16 +4,17 @@ let dom, reloj, roscoActivo, partida;
 function iniciarJuego() {
     var urlParams = new URLSearchParams(window.location.search);
     var esFormulario = urlParams.has('form');
-
     const jugador1 = localStorage.getItem('jugador1');
     const jugador2 = localStorage.getItem('jugador2');
     const segundos = localStorage.getItem('segundos');
     const comodines = localStorage.getItem('comodines');
-    
+
     if (!esFormulario) {
         const redirect = window.location.href.replace('rosco.html', 'index.html').replace(window.location.search, '')
         window.open(redirect, '_self');
     }
+
+    history.replaceState(null, '', window.location.href.replace(window.location.search, '?form=1'));
 
     dom = new DOM();
     reloj = new Reloj();
